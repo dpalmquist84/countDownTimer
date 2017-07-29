b1873a12b5ea48fa91c485fd801293f095e75af1
David Palmquist <dpalmquist84@gmail.com>
2017-07-28 21:17:07 -0400

    made the buttons work correctly and updated the time to digitial

--- assets/javascript/clock.js
+++ assets/javascript/clock.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
 var audio = new Audio("./assets/audio/alarm.mp3");
-var time = 1500;
+var time = 5;
 var intervalId;
+minutes = 24;
+seconds = 60;
 
 
 $("#stop").on("click", stop);
@@ -10,29 +12,47 @@ $("#start").on("click", run);
 $("#reset").on("click", reset);
 
 function run() {
+	audio.pause();
 	intervalId = setInterval(decrement, 1000);
+	if (minutes === 0 && seconds === 0 ) {
+		audio.play();
+	}
 
 }
 
 function decrement(){
-	time--
+	seconds--;
 
-$("#display").html("<h4>" + time + "</h4>");
+$("#display").html(minutes + ":" + seconds);
+    if (seconds < 10 && seconds > 0) {
+    	$("#display").html(minutes + ":0" + seconds)
 
-	if(time === 0){
+          
+    }
+	else if(seconds === 0){
+		$("#display").html(minutes + ":00")
+		minutes--;
+		seconds = 60;
+	} else if (seconds === 0 && minutes === 0 ) {
 		audio.play();
+		setTimeout(stop)
 		stop();
+		start();
 	}
 }
 
 function stop() {
+	audio.pause();
 	clearInterval(intervalId);
 }
 
 function reset() {
-	time = 1500;
-	$("#display").html("<h4>" + time + "</h4>");
-
+	stop();
+	$("#display").html("25:00");
+	minutes = 25;
+	seconds = 60;
+	run();
+    
 }
 // run();
 
